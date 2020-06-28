31d52fc7b09408e98657174944f9041667ca2cd5
Dhrumi Kansara <dhrumikansara1999@gmail.com>
2020-06-28 14:23:03 +0530

    final commit

--- app.js
+++ app.js
@@ -2,11 +2,11 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const date = require(__dirname + "/date.js");
 const app=express();
 
-let items=[];
-let workItems=[];
+const items=[];
+const workItems=[];
 
 app.set('view engine','ejs');
 
@@ -14,21 +14,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static("public"));
 
-const today=new Date();
-const currentDay=today.getDay();
-let day;
-let options={};
-app.get("/",function(req, res){
-  
-  
-  
-  options={
-    weekday: "long",
-    day:"numeric",
-    month:"long"
-  };
-  day=today.toLocaleDateString("en-US",options);
 
+app.get("/",function(req, res){
+  let day=date.getDate();
   res.render("list", {listType: day, newListItems: items});
 }); 
 
@@ -36,10 +24,12 @@ app.get("/work",function(req, res){
   res.render("list",{listType:"work", newListItems: workItems});
 });
 
-let item;
+
+
 app.post("/",function(req, res){
-  item=req.body.item;
-  if(req.body.listType="work") {
+  
+  let item=req.body.item;
+  if(req.body.listType=="work") {
     workItems.push(item);
     res.redirect("work");
   }
@@ -51,9 +41,9 @@ app.post("/",function(req, res){
   }
 });
 
-let workItem;
+
 app.post("/work",function(req, res){
-  item=req.body.item;
+  let item=req.body.item;
   items.push(item);
   res.redirect("/work");
 });
