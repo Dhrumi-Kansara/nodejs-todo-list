d8585ea738ad39ffe2dd29e6aeeb76bf2d8b928d
Dhrumi Kansara <dhrumikansara1999@gmail.com>
2020-07-01 20:16:15 +0530

    added database and a custom to do list path

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const _ = require("lodash");
+
 const date = require(__dirname + "/date.js");
 
 const app=express();
@@ -15,50 +17,102 @@ app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/todolistDB", {
   useNewUrlParser: true,
-  useUnifiedTopology: true 
+  useUnifiedTopology: true,
+  useFindAndModify: false 
 });
 
 const itemsSchema = new mongoose.Schema({
   name: String
 });
 
+const listSchema = new mongoose.Schema({
+  name: String,
+  items: [itemsSchema]
+});
+
 const Item = new mongoose.model("Item",itemsSchema);
 
+const List = new mongoose.model("List",listSchema);
+
 app.get("/",function(req, res){
-  let day=date.getDate();
-  
+
   Item.find({},function(err, foundItems){
-    res.render("list", {listType: day, newListItems: foundItems});
-    console.log(foundItems);
+
+  res.render("list", {listType: "Today", newListItems: foundItems});
+
   });  
-  
 }); 
 
-app.post("/",function(req, res){
+app.get("/:customListName",function(req, res){
   
+  const customListName = _.capitalize(req.params.customListName);
+
+  List.findOne({name: customListName},function(err, foundList) {
+    
+    if(!err) {
+      if(!foundList) {
+        
+        const newList = new List({
+          name: customListName,
+          items: []
+
+        });
+        newList.save(); 
+        res.redirect("/"+customListName);
+      }
+      else {
+        res.render("list", {listType: foundList.name, newListItems: foundList.items});
+      }
+    }
+  });
+});
+
+
+app.post("/",function(req, res){
+
+  let listType = req.body.listType;
   let itemName=req.body.item;
+
   
   const newItem = new Item({
     name: itemName
   });
-  newItem.save();
-  res.redirect("/");
-  
+
+  if(listType=="Today") {
+
+    newItem.save();
+    res.redirect("/");
+  }
+  else {
+    List.findOne({name: listType},function(err, foundList) {
+      foundList.items.push(newItem);
+      foundList.save();
+      res.redirect("/"+listType);
+    });
+  }
 });
 
 app.post("/delete",function(req, res){
   const checkedItemId=req.body.checkBox;
-
-  Item.findByIdAndRemove(checkedItemId,function(err){
-    if(err) {
-      console.log(err);
-    }
-    else {
-      console.log("delete successfull");
-      res.redirect("/");
-    }
-  });
-
+  const listType=req.body.listType;
+  if(listType=="Today") {
+    Item.findByIdAndRemove(checkedItemId,function(err){
+      if(err) {
+        console.log(err);
+      }
+      else {
+        console.log("delete successfull");
+        res.redirect("/");
+      }
+    });
+  }
+  else {
+    List.findOneAndUpdate({name: listType},{$pull: {items: {_id: checkedItemId}}},function(err, foundList) {
+      if(!err) {
+        res.redirect("/" + listType);
+      }
+    });
+  }
 });
 
 app.listen(3000, function() {
